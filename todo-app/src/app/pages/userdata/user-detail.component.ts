77931346a85b70980e3fdadf276a91b51908a985
Sebastian Klingbeil <sebastian.klingbeil@photonic-codes.com>
2022-10-19 16:01:09 +0200

    Deaktivierter Knopf wenn nichts im Inputfeld "Benutzerdaten"

--- todo-app/src/app/pages/userdata/user-detail.component.ts
+++ todo-app/src/app/pages/userdata/user-detail.component.ts
@@ -24,50 +24,46 @@ export class UserDetailComponent implements OnInit {
   }
 
   pics: Picture[] = [];
-  loginUser = this.loginService.currentUser.name;
-
-  loggenInUserID = this.loginService.currentUser.id;
-  loggedInUserName = this.loginService.currentUser.username;
-  loggedInEmail = this.loginService.currentUser.email;
+  // Variables
+  loginUser = this.loginService.currentUser?.name;
+  loggenInUserID = this.loginService.currentUser?.id;
+  loggedInUserName = this.loginService.currentUser?.username;
+  loggedInEmail = this.loginService.currentUser?.email;
   profileImage = ''; //this.pics[0].thumbnailUrl;
+  isDisabled = true;
 
+  // Getter and setter changeUsername
   private _changeUsername = ''; //ngModel
   // get - set Username
   public get changeUsername() {
     return this._changeUsername;
   }
   public set changeUsername(value) {
-
     // Prüfen, ob vorher noch nie etwas eingegeben wurde ("pristine")
-    if(value.length <= 0){
-      console.log('NOTHING')
+    if (value.length <= 0) {
       this.isDisabled = true;
-    }
-    else if(value.length >= 0) {
+    } else if (value.length >= 0) {
       this.isDisabled = false;
     }
     // Hier soll der Button aktiviert werden
     this._changeUsername = value;
   }
-
+  // Getter and setter changeEmail
   private _changeEmail = ''; //ngModel
   public get changeEmail() {
     return this._changeEmail;
   }
   public set changeEmail(value) {
     // Prüfen, ob vorher noch nie etwas eingegeben wurde ("pristine")
-    if(value.length <= 0){
-      console.log('NOTHING')
+    if (value.length <= 0) {
       this.isDisabled = true;
-    }
-    else if(value.length >= 0) {
+    } else if (value.length >= 1) {
       this.isDisabled = false;
     }
     this._changeEmail = value;
   }
-  isDisabled = true;
 
-  checkChanges(){
+  checkChanges() { // Disable the "Save" button if no changes are made
     this.changeUsername;
     this.changeEmail;
   }
